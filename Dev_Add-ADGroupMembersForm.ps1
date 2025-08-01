d6f44476b2aa8d76d55dd1186e0ffaae546a3d08
Josh Breeds <138309638+joshbreeds@users.noreply.github.com>
2025-08-01 10:16:43 +0100

    Update Dev_Add-ADGroupMembersForm.ps1

--- Dev_Add-ADGroupMembersForm.ps1
+++ Dev_Add-ADGroupMembersForm.ps1
@@ -1,1 +1,16 @@
+New-UDForm -Title "Add Users to AD Group" -Content {
+    New-UDTextbox -Id "txtFilePath" -Label "Path to User Names File (.txt)"
+    New-UDTextbox -Id "txtGroupName" -Label "AD Group Name"
+} -OnSubmit {
+    $FilePath = $EventData.txtFilePath
+    $GroupName = $EventData.txtGroupName
+
+    # Run the script with parameters
+    $Output = Invoke-UAScript -Name "Add-ADGroupMembers.ps1" -Parameters @{
+        UserNamesFile = $FilePath
+        ADGroup       = $GroupName
+    }
+
+    Show-UDToast -Message "Script started. Monitor jobs for progress." -Duration 5000
+}
 
